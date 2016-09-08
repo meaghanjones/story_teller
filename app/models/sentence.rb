94e58103181f50a0bbbf0351c67fb2b2088581da
Katarina Tuttle and Meaghan Jones <katarina+meaghan@>
2016-09-08 14:08:09 -0700

    set maximum of 100 characters for contant of sentence

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -2,5 +2,6 @@ class Sentence<ActiveRecord::Base
   belongs_to :story
 
   validates :username, :presence => true
-  validates :content, :presence => true
+  validates :content, :presence => true, length:{maximum: 100}
+
 end
